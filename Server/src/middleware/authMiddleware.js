fac76cc0bd713b3dbb98c3f860562a3e9a7be34d
Deepak Narve <deepaknarve8989@gmail.com>
2025-12-17 15:29:26 +0530

    fix the authmiddleware and authcontroller

--- Server/src/middleware/authMiddleware.js
+++ Server/src/middleware/authMiddleware.js
@@ -1,6 +1,8 @@
+const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const Role = require("../models/roleModel");
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
@@ -13,6 +15,14 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
+
+      // Manually populate role if it is an ObjectId
+      if (req.user && req.user.role && mongoose.Types.ObjectId.isValid(req.user.role)) {
+        const roleDoc = await Role.findById(req.user.role);
+        if (roleDoc) {
+          req.user.role = roleDoc;
+        }
+      }
       next();
     } catch (error) {
       res.status(401);
